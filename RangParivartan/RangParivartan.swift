824768efcfb712cdb0609e83782d2596bc8ddd16
Ravi Kushwaha <ravi.kushwaha@nagarro.com>
2020-06-02 00:00:46 +0530

    Added rangParivartan.swift file

---
+++ RangParivartan/RangParivartan.swift
@@ -0,0 +1,23 @@
+//
+//  RangParivartan.swift
+//  RangParivartan
+//
+//  Created by Ravi Shankar Kushwaha on 01/06/20.
+//  Copyright © 2020 Self. All rights reserved.
+//
+
+import Foundation
+
+public final class RangParivartan {
+
+    let name = "RangParivartan"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- RangParivartanTests/RangParivartanTests.swift
+++ RangParivartanTests/RangParivartanTests.swift
@@ -19,16 +19,14 @@ class RangParivartanTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    var rangParivartan: RangParivartan!
+
+    override func setUp() {
+        rangParivartan = RangParivartan()
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testAdd() {
+        XCTAssertEqual(rangParivartan.add(a: 1, b: 1), 2)
     }
 
 }
