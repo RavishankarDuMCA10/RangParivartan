b5477783010041eb4d43743dfe164aef6bffdcb8
Ravi Kushwaha <ravi.kushwaha@nagarro.com>
2020-06-01 23:43:49 +0530

    Initial project setup

---
+++ RangParivartan/RangParivartan.h
@@ -0,0 +1,19 @@
+//
+//  RangParivartan.h
+//  RangParivartan
+//
+//  Created by Ravi Shankar Kushwaha on 01/06/20.
+//  Copyright © 2020 Self. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RangParivartan.
+FOUNDATION_EXPORT double RangParivartanVersionNumber;
+
+//! Project version string for RangParivartan.
+FOUNDATION_EXPORT const unsigned char RangParivartanVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RangParivartan/PublicHeader.h>
+
+
---
+++ RangParivartanExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RangParivartanExamples
+//
+//  Created by Ravi Shankar Kushwaha on 01/06/20.
+//  Copyright © 2020 Self. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
